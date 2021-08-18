7406964c20f5b8638a033acf6c2f581bfe2d9566
Bohdan11Dii <bodyabrovdiy@gmail.com>
2021-08-18 14:13:41 +0300

    add button

--- First_project/Dota_2/dota/models.py
+++ First_project/Dota_2/dota/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Heroy(models.Model):
@@ -9,3 +10,7 @@ class Heroy(models.Model):
 
     def __str__(self):
         return self.title
+
+
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'post_id': self.pk})
---
+++ First_project/Dota_2/dota/utils.py
@@ -0,0 +1,17 @@
+menu = [{'title': 'Home', 'url_name': 'home'},
+        {'title': 'About', 'url_name': 'about'},
+        {'title': 'Contact', 'url_name': 'contact'},
+
+        ]
+
+
+
+class DataMixin:
+    def get_user_context(self, **kwargs):
+        context = kwargs
+        # cats = Category.objects.all()
+        context['menu'] = menu
+        # context['cats'] = cats
+        if 'cat_selected' not in context:
+            context['cat_selected'] = 0
+        return context
